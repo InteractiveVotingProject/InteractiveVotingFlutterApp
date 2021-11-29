8f5dcd5027c3a925f26be5bd893d9ce4fee6c0d7
ThomasMarguet <thommarg@hotmail.fr>
2021-11-29 14:52:00 +0100

    Add quizz creation and qr generation

--- interactive_voting_flutter_app/lib/main.dart
+++ interactive_voting_flutter_app/lib/main.dart
@@ -1,45 +1,9 @@
 import 'package:flutter/material.dart';
+import 'Homepage.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Application home'),
-          ),
-          body: Center(
-              child: Column(children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(25),
-              child: OutlinedButton(
-                child: Text(
-                  "Launch a quiz",
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                onPressed: () {},
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(25),
-              child: OutlinedButton(
-                child: Text(
-                  'Answer',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                onPressed: () {},
-              ),
-            ),
-          ]))),
-    );
-  }
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: Homepage(),
+  ));
 }
